da14ba5d0a1faececc5a931daeca7d36f94ceeae
Aayush <aayush.paigwar123@gmail.com>
2023-12-07 22:09:26 +0530

    Integrated the Supabase DB to flutter app ✅

---
+++ android/app/src/main/kotlin/com/example/supa_base/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.supa_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: "assets/.env");
+
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_KEY'),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Countries',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client
+      .from('countries')
+      .select<List<Map<String, dynamic>>>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder<List<Map<String, dynamic>>>(
+          future: _future,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final countries = snapshot.data!;
+            return ListView.builder(
+              itemCount: countries.length,
+              itemBuilder: ((context, index) {
+                final country = countries[index];
+                return ListTile(
+                  title: Text(country['name']),
+                );
+              }),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
