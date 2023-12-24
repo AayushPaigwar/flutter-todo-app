f34a670fa6625ac378e41516582048885feacb72
Aayush <aayush.paigwar123@gmail.com>
2023-12-25 00:07:08 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'TODO App',
+      title: 'TODO App Supabase',
       home: HomePage(),
     );
   }
